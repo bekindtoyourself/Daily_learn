4bf6b420d7718789fcb3d6330af2ea370740f5a8
bekindtoyourself <12084895+bekindtoyourself@users.noreply.github.com>
2019-04-08 17:39:28 +0800

    add django file

--- Ecommerce_website_dev/apps/user/models.py
+++ Ecommerce_website_dev/apps/user/models.py
@@ -3,3 +3,34 @@ from django.db import models
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
 from db.base_model import BaseModel
+
+class User(AbstractUser, BaseModel):
+    class Meta:
+        db_table = 'df_user'
+        verbose_name = '用户'
+        verbose_name_plural = verbose_name
+
+class AddressManager(models.Manager):
+
+    def get_default_address(self, user):
+        try:
+            address = self.model.objects.get(user=user, is_default=True)
+        except self.model.DoesNotExist:
+            address = None
+
+        return address
+
+class Address(BaseModel):
+    user = models.ForeignKey('User', verbose_name='所属账号')
+    receiver = models.CharField(max_length=20, verbose_name='收件人')
+    addr = models.CharField(max_length=256, verbose_name='收件地址')
+    zip_code = models.CharField(max_length=6, null=True, verbose_name='邮政编码')
+    phone = models.CharField(max_length=11, verbose_name='联系电话')
+    is_default = models.BooleanField(default=False, verbose_name='是否默认')
+
+    objects = AddressManager()
+
+    class Meta:
+        db_table = 'df_address'
+        verbose_name = '地址'
+        verbose_name_plural = verbose_name
